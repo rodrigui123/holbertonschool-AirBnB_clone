4b886af97193fae04f832f1df6cc73b5277ffadd
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-12 13:58:20 -0300

    Some fixes

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,6 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
-        self.number_of_instances += 1
 
     def __str__(self):
         print(f"[{type(self)}] ({self.id}) {self.__dict__}")
