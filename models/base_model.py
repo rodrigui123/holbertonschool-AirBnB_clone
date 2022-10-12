fa4aa283b97d375b9950004930fc1bca8682d221
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-12 13:47:02 -0300

    WIP: Create BaseModel class.

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+import datetime
+from uuid import uuid4
+import uuid
+
+
+class BaseModel:
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        print(f"[{type(self)}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        return self.__dict__
