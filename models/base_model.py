a427b16df2121fc3534ee2f935588fab16d68383
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-12 13:48:24 -0300

    Fixes in BaseModel class.

--- models/base_model.py
+++ models/base_model.py
@@ -7,14 +7,14 @@ import uuid
 class BaseModel:
     def __init__(self):
         self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         print(f"[{type(self)}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         return self.__dict__
