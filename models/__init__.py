a9980435ce14ecfbc4e237dc2f70fd1a3408d6d1
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-12 16:46:43 -0300

    Fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """python interpreter"""
 
-from engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
