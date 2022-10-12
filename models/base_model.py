7c50bd92cf5a78a32090e7efb2b8473d65fca359
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-12 14:04:48 -0300

    Fix __init__ in BaseModel Class.

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,8 @@ class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         print(f"[{type(self)}] ({self.id}) {self.__dict__}")
