881d390fbfca846b9176ad1955ef30b7e1f88888
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-12 17:12:51 -0300

    Fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,23 +10,22 @@ class FileStorage():
     __objects = {}
 
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
     
     def new(self, obj):
-        self.__objects[obj.id] = obj
+        FileStorage.__objects[obj.id] = obj
 
     def save(self):
         objs = {}
-        for id in self.__objects:
-            objs[id] = self.__objects[id].to_dict()
-        with open(self.__file_path, 'w') as f:
+        for id in FileStorage.__objects:
+            objs[id] = FileStorage.__objects[id].to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
             f.write(json.dumps(objs))
 
     def reload(self):
-        self.__objects = {}
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r') as f:
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r') as f:
                 file_content = f.read()
                 data = json.loads(file_content) if file_content is not None else []
                 for key, value in data.items():
-                    self.__objects[key] = BaseModel(value)
+                    FileStorage.__objects[key] = BaseModel(value)
