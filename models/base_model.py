d7a844dbd824a4c1fb312b5b3838040e63cc1015
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-12 13:57:44 -0300

    Fixes

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,9 @@ import uuid
 
 
 class BaseModel:
-    number_of_instances = 0
 
     def __init__(self):
-        self.id = int(uuid.uuid4()) + self.number_of_instances
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
         self.number_of_instances += 1
