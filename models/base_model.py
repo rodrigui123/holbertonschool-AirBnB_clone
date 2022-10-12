bd08b64aacc8c46c41c6965a2d03ea31ec8cfb73
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-12 14:03:37 -0300

    FIx created_at and updated_at attribute in BaseModel class.

--- models/base_model.py
+++ models/base_model.py
@@ -8,14 +8,14 @@ class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
 
     def __str__(self):
         print(f"[{type(self)}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now().isoformat()
 
     def to_dict(self):
         return self.__dict__
