fa05b74705eadb1e1f47b0ee8e4f6f7148a601b2
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-12 14:54:06 -0300

    Fixes

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import datetime
+from datetime import datetime
 from uuid import uuid4
 import uuid
 
@@ -12,19 +12,19 @@ class BaseModel:
                 if key == 'id':
                     self.id = value
                 elif key == 'created_at':
-                    self.created_at = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.created_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == 'updated_at':
-                    self.updated_at = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.updated_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         temp = {}
