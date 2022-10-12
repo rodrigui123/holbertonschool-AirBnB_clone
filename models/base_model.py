d6f91ee9b73ee352d6b3982f51a4a0bf6c2fc48c
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-12 14:48:55 -0300

    Update __init__ method.

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,19 @@ import uuid
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(value)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
