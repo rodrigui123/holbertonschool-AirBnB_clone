4623ad8b775ee2a8157bdaa13cf202a72db47f95
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-17 21:57:49 -0300

    Create test to BaseModel.

---
+++ models/tests/test_base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import os
+import unittest
+from models.base_model import BaseModel
+
+
+class BaseModelTest(unittest.TestCase):
+    def test_save(self):
+        base = BaseModel()
+        created_at = base.created_at
+        update_at = base.updated_at
+        base.save()
+        self.assertNotEqual(update_at, base.updated_at)
+        self.assertEqual(created_at, base.created_at)
+
+    def test_to_dict(self):
+        base = BaseModel()
+        temp = {}
+        temp['id'] = self.id
+        temp['__class__'] = self.__class__.__name__
+        temp['created_at'] = self.created_at.isoformat()
+        temp['updated_at'] = self.updated_at.isoformat()
+        self.assertEqual(temp, base.to_dict())
+
+    def test_save(self):
+        base = BaseModel()
+        try:
+            os.remove('file.json')
+        except Exception:
+            pass
+        base.save()
+        self.assertTrue(os.path.exists('file.json'))
