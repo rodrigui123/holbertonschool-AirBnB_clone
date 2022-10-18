fe66d95fd3a96de64c584b7f09efc0ba5b8a5122
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-17 22:47:11 -0300

    Fix

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,13 +1,8 @@
-from msilib.schema import File
 import unittest
 from models.engine.file_storage import FileStorage
 
 
 class FileStorageTest(unittest.TestCase):
-    def test_path(self):
-        storage = FileStorage()
-        self.assertEqual(storage.__file_path, "file.json")
-
     def test_reload(self):
         storage = FileStorage()
         storage.all().clear()
