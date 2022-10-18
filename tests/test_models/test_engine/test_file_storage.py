455917b5f23182df6b6ef0b2f900f08f340c5256
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-17 22:49:32 -0300

    Fix test file_storage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,10 @@ from models.engine.file_storage import FileStorage
 
 
 class FileStorageTest(unittest.TestCase):
+    def test_path(self):
+        storage = FileStorage()
+        self.assertEqual(storage._FileStorage__file_path, "file.json")
+
     def test_reload(self):
         storage = FileStorage()
         storage.all().clear()
